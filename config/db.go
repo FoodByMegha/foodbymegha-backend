dbdf7b9662235cfbc0360bd5e12ed9c2e6656541
unknown <tumhari@email.com>
2026-05-02 23:50:50 +0530

    Feat: db.go sslmode=require update

--- config/db.go
+++ config/db.go
@@ -22,7 +22,7 @@ func ConnectDB() {
 
 	// Connection string banao
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
 		host, port, user, password, dbname,
 	)
 
@@ -41,6 +41,4 @@ func ConnectDB() {
 	DB = db
 	log.Println("Database connected aur tables ready! 🎉")
 
-
 }
-
